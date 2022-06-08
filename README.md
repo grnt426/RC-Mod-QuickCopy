64fa37e0203260bb278a5b7527fe99e9248c380c
Grant Kurtz <kurtz.grant@gmail.com>
2022-06-08 13:08:53 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Quick Copy
+This mod allows you to quickly copy stuff on screen to your clipboard for pasting into chat!
+
+# Usage
+## System Position
+Click on a system so it is full screen. Then, type in `/claim` in chat to have its position copied to your clipboard, like this 
+
+`system BAMA (237, 120) in Galvanie`
+
+You can then paste this in your faction's system-reservation chat, spotted enemy chat, etc.
+
+# Install
+Dump this file into `Rising Constellation/dist/main`. Make sure to also have its dependency, [RCMA](https://github.com/grnt426/RC-Mod-API).
---
+++ quickcopy_mod.js
@@ -0,0 +1,57 @@
+class QuickCopy {
+    constructor() {
+        this.matcher = /^claim/;
+    }
+
+    chatMessage(message) {
+        if(this.matcher.test(message)) {
+            try {
+                let selected = window.gamestate.game.selectedSystem;
+                if(selected) {
+                    let preface = "system";
+                    if(selected.status === "inhabited_player") {
+                        if(selected.owner.faction_id !== window.gamestate.game.player.faction_id) {
+                            preface = "enemy " + preface;
+                        }
+                        else {
+                            window.granite.debug("That system is owned by your team! " + selected.name);
+                        }
+                    }
+                    else if(selected.status === "inhabited_neutral") {
+                        preface = "dominion";
+                    }
+                    else if(selected.status === "inhabited_dominion") {
+                        if(selected.owner.faction_id !== window.gamestate.game.player.faction_id) {
+                            preface = "enemy " + preface;
+                        }
+                        else {
+                            window.granite.debug("That system is owned by your team! " + selected.name);
+                        }
+                    }
+
+                    let position = selected.position;
+                    let x = Math.round(position.x);
+                    let y = Math.round(position.y);
+                    let sector = window.gamestate.game.galaxy.sectors[selected.sector_id];
+                    let content = `${preface} ${selected.name.toUpperCase()} (${x}, ${y}) in ${sector.name}`;
+                    navigator.clipboard.writeText(content).then(r => {
+                        if(r) {
+                            window.granite.debug("Failed to write to clipboard: " + r, window.granite.levels.ERROR);
+                        }
+                    });
+                }
+                else {
+                    // nothing selected? Maybe call out enemies in area?
+                }
+            }
+            catch(err) {
+                window.granite.debug("Failed to parse system lookup: " + err, window.granite.levels.ERROR);
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
+
+window.granite.addHookListener(new QuickCopy());
